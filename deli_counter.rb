125b00ec95a618ba40356c35119a51c214d5fcba
Donovan Dwyer <dwyer.donovan@gmail.com>
2018-09-09 17:40:42 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,11 @@ def line(arr)
   if arr.length == 0 
     puts "The line is currently empty"
   elsif
+    line = "The line is currently: "
     arr.each_with_index do |name, index|
-      
+      line += "#{index + 1}. #{name} "
+    end
+    line
+  end
 end
+
